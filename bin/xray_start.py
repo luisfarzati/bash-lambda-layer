392594bd06cc4b94b2028a2f17ed8f6cef94d745
Luis Farzati <lfarzati@gmail.com>
2019-12-22 04:10:16 +0100

    trying to connect traces

--- bin/xray_start.py
+++ bin/xray_start.py
@@ -6,10 +6,9 @@ import binascii
 
 AWS_LAMBDA_FUNCTION_NAME=os.getenv("AWS_LAMBDA_FUNCTION_NAME")
 AWS_XRAY_DAEMON_ADDRESS=os.getenv("AWS_XRAY_DAEMON_ADDRESS")
+TRACE_ID=sys.argv[1] # os.getenv("AWS_LAMBDA_TRACE_ID")
 
 START_TIME = time.time()
-HEX=hex(int(START_TIME))[2:]
-TRACE_ID="1-" + HEX + "-" + binascii.b2a_hex(os.urandom(12))
 SEGMENT_ID=binascii.b2a_hex(os.urandom(8))
 SEGMENT_DOC=json.dumps({"trace_id": TRACE_ID, "id": SEGMENT_ID, "start_time": START_TIME, "in_progress": True, "name": AWS_LAMBDA_FUNCTION_NAME})
 HEADER=json.dumps({"format": "json", "version": 1})
